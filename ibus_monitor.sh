3e8ac97f16577510b469918403239b96c4910f90
banbeucmas <banbeucams@gmail.com>
2022-06-10 17:52:47 +0900

    Error message pipes

--- ibus_monitor.sh
+++ ibus_monitor.sh
@@ -4,10 +4,8 @@ addr=$(ibus address)
 entry="destination='org.freedesktop.IBus',interface='org.freedesktop.IBus',member='SetGlobalEngine'"
 lang="$(ibus engine)"
 
-(sleep 1 && echo $lang) &
-
 while true; do
-    dbus-monitor --monitor --address $addr $entry | 
+    dbus-monitor --monitor --address $addr $entry 2>/dev/null | 
     while read -r sign; do
         if [[ $sign == string* ]]; then
             echo $sign | grep -o '"[^"]\+"' | sed 's/"//g'
