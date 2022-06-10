de79a8d198677e83cd3ac9fd16b854d8df71b7d7
banbeucmas <banbeucams@gmail.com>
2022-06-10 17:34:48 +0900

    echo delay

--- ibus_monitor.sh
+++ ibus_monitor.sh
@@ -4,6 +4,8 @@ addr=$(ibus address)
 entry="destination='org.freedesktop.IBus',interface='org.freedesktop.IBus',member='SetGlobalEngine'"
 lang="$(ibus engine)"
 
+(sleep 1 && echo $lang) &
+
 while true; do
     dbus-monitor --monitor --address $addr $entry | 
     while read -r sign; do
